ebcb36704cab3e40bf1ccf400ed61ebe86284022
Gaurav <gaurav@mortgagekart.com>
2023-02-10 15:37:04 +0530

    crud function cli app

--- app.js
+++ app.js
@@ -1,178 +1,124 @@
 const http = require('http');
-const net = require('net')
-const { URL } = require('url')
 const btoa = require('btoa');
-
+const prompt = require('prompt-sync')();
 
 const kintoCredentials = 'admin:mortgagekart';
 const secreString = btoa(kintoCredentials);
 // const remoteUrl = "http://localhost:8888/v1/";
 
-const option = {
+
+var options = {
     port: 8888,
     host: '127.0.0.1',
-    path:'/v1/buckets/blog/collections/articles/records/968f65f9-a81c-4670-803f-88293e055677',
+    path:'/v1/buckets/blog/collections/articles/records',
     headers:{
         'Authorization': "Basic "+ secreString
     }
 }
 
-let request = http.get(option, (res)=>{
+function createClient(){
 
-    if (res.statusCode !== 200) {
-        console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
-        res.resume();
-        return;
+    const client = http.request(options,(res)=>{
 
-      }
-    let data = '';
-    console.log(data);
-
-    res.on('data', (chunk) => {
-        data += chunk;
+        let data = '';
         console.log(data);
+
+        res.on('data', (chunk) => {
+            data += chunk;
+            console.log(data);
+        });
+
+        res.on('close', () => {
+
+        console.log('Retrieved all data');
+        console.log(JSON.parse(data));
+
+        });
     });
 
-    res.on('close', () => {
+    return client;
 
-    console.log('Retrieved all data');
-    console.log(JSON.parse(data));
+}
 
-    });
-});
-// 
-const auth =  request.getHeader('Authorization')
+// read data GET record
+function readRecords(){
 
-console.log(auth);
+    options.method='GET';
+    console.log("This is option",options);
 
+    client = createClient();
+        
+}
 
 //post article1 and records
+//********* */
 
-// const option1 = {
-//     method:'POST',
-//     port: 8888,
-//     host: '127.0.0.1',
-//     path:'/v1/buckets/blog/collections/articles/records/',
-//     headers:{
-//         'Authorization': "Basic "+ secreString
-//     }
-// }
+function postArticle(){
 
-// console.log(option1);
+    const jsondata = {
+        "data": {title: "My fifth article", content: "Using node http!"}
+    }
 
-// const jsondata = {
-//     "data": {title: "My first article", content: "Using node http!"}
-// }
+    options.method = 'POST';
+    client=createClient();
 
-// let req = http.request(option1, (res)=>{
-
-//     // if (res.statusCode !== 200) {
-//     //     console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
-//     //     res.resume();
-//     //     return;
-//     // }
-//     console.log(`STATUS: ${res.statusCode}`);
-//     // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-//     console.log("im here");
-
-//     res.on('data',(chunk)=>{
-//         console.log(`BODY:${chunk}`);
-//     });
-
-//     res.on('close', () => {
-//     console.log('done');
-//     })
-
-// });
-
-// req.on('error', (e) => {
-//     console.error(`problem with request: ${e.message}`);
-//   });
-
-// req.write(JSON.stringify(jsondata))
-// req.end();
-
+    client.write(JSON.stringify(jsondata))
+}
 
 // //update record of blog/article
-// const option2 = {
-//     method:'PUT',
-//     port: 8888,
-//     host: '127.0.0.1',
-//     path:'/v1/buckets/blog/collections/articles/records/968f65f9-a81c-4670-803f-88293e055677',
-//     headers:{
-//         'Authorization': "Basic "+ secreString
-//     }
-// }
 
-// console.log(option2);
+function updateRecord(){
 
-// const updatedData = {
-//     "data": {title: "My second article", content: "Using node http!"}
-// }
+    const updatedData = {
+        "data": {title: "My fifth updated article"}
+    }
+    options.method='PUT';
+    options.path = options.path.concat("/46c580c7-7e7e-4b04-8513-44b34750f660");
+    console.log(options.path);
+    client=createClient();
+    client.write(JSON.stringify(updatedData));
+    
+}
 
-// const req = http.request(option2,(res)=>{
+// delete record
 
-//     console.log(`STATUS: ${res.statusCode}`);
-//     res.on('data',(chunk)=>{
-//                 console.log(`BODY:${chunk}`);
-//             });
-        
-//     res.on('close', () => {
-//     console.log('done');
-//     })
+function deleteArticle(){
 
-// })
-// req.write(JSON.stringify(updatedData));
-// req.end()
+    options.method='DELETE';
+    options.path = options.path.concat("/46c580c7-7e7e-4b04-8513-44b34750f660");
+    console.log(options.path);
+    client=createClient();
+    
+}
 
 
+//Functions
 
+console.log("1.Create\n2.Read\n3.Update\n4.Delete");
+var option = prompt("Select operation to perform ");
 
+console.log(option);
+switch(option){
 
+    case "1":
+        console.log("Creating article using json provided in code");
+        postArticle();
+        break;
 
+    case "2":
+        console.log("Read");
+        readRecords();
+        break;
 
+    case "3":
+        console.log("Update");
+        updateRecord();
+        break;
 
+    case "4":
+        console.log("Delete");
+        deleteArticle();
+        break;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // const result = request.createBucket("new");
-
-// // console.log(result);    
-
-
-
-// // async function fun(){
-
-// //     const res = await request.bucket("blog").collection("posts")
-// //     .getRecord("849a9adb-7755-472c-b998-f0d7d24a6218");
-
-// //     // const updated = {
-// //     //     id: "cb0f7b2b-e78f-41a8-afad-92a56f8c88db",
-// //     //     title: "My first post, edited",
-// //     //     content: "Hello World, again!"
-// //     //   };
-      
-// //     //   const result = await client.bucket("blog").collection("posts")
-// //     //     .updateRecord(updated);
-
-// // //     const result = await client.bucket("blog").collection("posts")
-// // //   .deleteRecord("849a9adb-7755-472c-b998-f0d7d24a6218");
-
-// //     console.log(res);
-
-// // }
-// // fun()
-
+client.end()
